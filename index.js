a896862fb0dd84481fa82e5cb0e9850265a7b9db
Jorgec88 <j.carralero88@gmail.com>
2023-12-20 11:18:28 -0500

    auth

--- index.js
+++ index.js
@@ -24,11 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-const allowedOrigins = [
-  'http://localhost:8080',
-  'http://testsite.com',
-  'http://localhost:1234'
-];
+const allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 app.use(
   cors({
