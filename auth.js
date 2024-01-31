63c70b5ab6e7741c5b0c4d975a73896364799645
Jorgec88 <j.carralero88@gmail.com>
2024-01-31 13:11:06 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -15,23 +15,21 @@ const generateJWTToken = (user) => {
 
 module.exports = (router) => {
   router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false },
-      (error, user, info) => {
-        console.log('Passport authenticated:', user);
-        if (error || !user) {
-          return res.status(400).json({
-            message: 'Something is not right',
-            user: user
-          });
-        }
-        req.login(user, { session: false }, (error) => {
-          if (error) {
-            res.send(error);
-          }
-          const token = generateJWTToken(user.toJSON());
-          return res.json({ user, token });
+    passport.authenticate('local', { session: false }, (error, user, info) => {
+      console.log('Passport authenticated:', user);
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user
         });
       }
-    )(req, res);
+      req.login(user, { session: false }, (error) => {
+        if (error) {
+          res.send(error);
+        }
+        const token = generateJWTToken(user.toJSON());
+        return res.json({ user, token });
+      });
+    })(req, res);
   });
 };
